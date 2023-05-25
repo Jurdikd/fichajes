cedf7b895bc1a7d5d0db1ea07d83caa7d50a1375
Jurdikd <jesuscovo@gmail.com>
2023-05-25 12:56:03 -0400

    Ajuste de filtro de colores segun el color

--- app/public/views/prubs.php
+++ app/public/views/prubs.php
@@ -11,7 +11,7 @@
 <style>
 .filterFichaje {
     filter: grayscale(100%) hue-rotate(60deg);
-
+    /* color: #f32545;*/
 }
 </style>
 
@@ -21,12 +21,47 @@
         <img class="img-fluid" src="<?php echo RUTA_IMG; ?>users/prub.JPG" alt="" srcset="" width="200" height="200">
         <img class="img-fluid filterFichaje" src="<?php echo RUTA_IMG; ?>users/prub.JPG" alt="" srcset="" width="200"
             height="200">
+        <br>
+        <img class="img-fluid" id="myImage" src="<?php echo RUTA_IMG; ?>users/prub.JPG" alt="Imagen" width="200"
+            height="200">
+        <input id="colorPicker" type="color">
     </div>
 
 
     <!-- javascript-->
     <script src="<?php echo RUTA_JS; ?>popper.min.js"></script>
     <script src="<?php echo RUTA_JS; ?>bootstrap.min.js"></script>
+    <script>
+    document.getElementById('colorPicker').addEventListener('input', function() {
+        var color = this.value;
+        var image = document.getElementById('myImage');
+        var filter = 'grayscale(1) sepia(1) saturate(1000%) hue-rotate(' + colorToHueRotation(color) + 'deg)';
+        image.style.filter = filter;
+    });
+
+    function colorToHueRotation(color) {
+        var hex = color.replace('#', '');
+        var r = parseInt(hex.slice(0, 2), 16);
+        var g = parseInt(hex.slice(2, 4), 16);
+        var b = parseInt(hex.slice(4, 6), 16);
+        var max = Math.max(r, g, b);
+        var min = Math.min(r, g, b);
+        var c = max - min;
+
+        var hue;
+        if (c === 0) {
+            hue = 0;
+        } else if (max === r) {
+            hue = ((g - b) / c) % 6;
+        } else if (max === g) {
+            hue = (b - r) / c + 2;
+        } else {
+            hue = (r - g) / c + 4;
+        }
+        hue = Math.round(hue * 60);
+        return hue;
+    }
+    </script>
 </body>
 
 </html>
