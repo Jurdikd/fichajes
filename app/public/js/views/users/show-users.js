799eb2476dc7eb7cd4eb800a4eb29f4d405fb405
Jurdikd <jesuscovo@gmail.com>
2023-06-28 19:24:54 -0400

    carga temporal de mejora de registro

--- app/public/js/views/users/show-users.js
+++ app/public/js/views/users/show-users.js
@@ -2,14 +2,15 @@ document.addEventListener("DOMContentLoaded", async () => {
 	showFichas();
 });
 
-const table = $("#tabla-usuarios").DataTable({
-	dom: "Bfrtip",
+const tablaUsuarios = $("#tabla-usuarios").DataTable({
+	//dom: '<"row"<"col-md-6 mb-1"l><"col-md-6"f><"col-md-6 text-md-end mt-1"B>>rtip',
+	dom: lfBrtip,
 	buttons: [
 		{
 			extend: "print",
 			text: "Imprimir PDF",
 			title: "",
-			filename: "Reporte de USUARIOS | FEDEAV", // Nombre del archivo
+			filename: "Reporte de USUARIOS | FEDEAV",
 			customize: function (win) {
 				// Agrega estilos CSS personalizados al documento de impresión
 				$(win.document.head).append(
@@ -19,7 +20,7 @@ const table = $("#tabla-usuarios").DataTable({
 				// Crea el contenido de la cabecera con la imagen y el texto centrado
 				const headerContent = `
 			<div class="header-content">
-			<h2>Federación Deportiva del Abogado de Venezuela XL Juegos Deportivos Nacionales Intercolegios de Abogados Merida 2023</h2>
+			  <h2>Federación Deportiva del Abogado de Venezuela XL Juegos Deportivos Nacionales Intercolegios de Abogados Merida 2023</h2>
 			  <img class="header-image" src="${RUTA_IMG}logo/logo-fedeav.JPG" alt="Logo FEDEAV" width="80" height="80" loading="lazy">
 			  <h2>Reporte de USUARIOS | FEDEAV</h2>
 			</div>
@@ -27,6 +28,9 @@ const table = $("#tabla-usuarios").DataTable({
 
 				// Agrega la cabecera al documento de impresión antes de la tabla
 				$(win.document.body).prepend(headerContent);
+
+				// Cambia el título de la pestaña del navegador
+				$(win.document).prop("title", "Reporte de USUARIOS | FEDEAV");
 			},
 			exportOptions: {
 				columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
@@ -40,16 +44,47 @@ const table = $("#tabla-usuarios").DataTable({
 						return data;
 					},
 				},
+				// Opción para imprimir solamente las entradas mostradas
+				rows: function (idx, data, node) {
+					const api = this.api();
+					const pageLength = api.page.len();
+					const currentPage = api.page();
+					const displayedIndex = currentPage * pageLength + idx;
+					return api.page.info().recordsDisplay > displayedIndex;
+				},
+			},
+			init: function (api, node, config) {
+				$(node).removeClass("btn-secondary").addClass("btn-primary");
 			},
 		},
 	],
 	responsive: true,
-	order: [],
 	lengthMenu: [
-		[5, 10, 20, 50, -1], // Opciones de cantidad de registros a mostrar
-		[5, 10, 20, 50, "Todos"], // Texto de las opciones
+		[6, 12, 24, 48, 120, -1],
+		[6, 12, 24, 48, 120, "Todos"],
 	],
-	iDisplayLength: -1, // Mostrar todos los registros
+	order: [],
+	language: {
+		paginate: {
+			previous: '<i class="bi bi-chevron-left"></i> Anterior',
+			next: 'Siguiente <i class="bi bi-chevron-right"></i>',
+		},
+		sEmptyTable: "No se encontraron datos",
+		sInfo: "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+		sInfoEmpty: "Mostrando 0 a 0 de 0 entradas",
+		sInfoFiltered: "(filtrado de _MAX_ entradas totales)",
+		sInfoPostFix: "",
+		sInfoThousands: ",",
+		sLengthMenu: "Mostrar _MENU_ entradas",
+		sLoadingRecords: "Cargando...",
+		sProcessing: "Procesando...",
+		sSearch: "Buscar:",
+		sZeroRecords: "No se encontraron registros coincidentes",
+		oAria: {
+			sSortAscending: ": activar para ordenar la columna de manera ascendente",
+			sSortDescending: ": activar para ordenar la columna de manera descendente",
+		},
+	},
 });
 
 const showFichas = async () => {
@@ -59,21 +94,14 @@ const showFichas = async () => {
 	if (solicitud) {
 		const datos = solicitud;
 
-		// Obtener referencia a la tabla
-		let tablaUsuarios = $("#tabla-usuarios").DataTable();
-
-		// Limpiar los datos existentes en la tabla
 		tablaUsuarios.clear().draw();
 
-		// Llenar la tabla con los datos obtenidos
 		datos.forEach((usuario) => {
 			tablaUsuarios.row
 				.add([
-					`
-                    <img class="img-fluid" src="${usuario.imagen}" alt="${
+					`<img class="img-fluid" src="${usuario.imagen}" alt="${
 						usuario.nombre + " " + usuario.apellido1
-					}" width="80" height="80" loading="lazy">
-                    `,
+					}" width="80" height="80" loading="lazy">`,
 					usuario.nombre + " " + usuario.nombre2,
 					usuario.apellido1 + " " + usuario.apellido2,
 					usuario.fecha_nacimiento,
@@ -81,30 +109,14 @@ const showFichas = async () => {
 					usuario.cedula,
 					usuario.codigo_empleado,
 					usuario.inpre_abogado,
-					`
-                   Télefono:
-				   <br>
-				   ${usuario.celular}
-				   <br>
-				   Correo:
-				   <br>
-				   ${usuario.correo}
-                    `,
+					`Télefono:<br>${usuario.celular}<br>Correo:<br>${usuario.correo}`,
 					usuario.nombre_rol,
 					usuario.estado_nom,
 					usuario.estado_nom,
-					`Edición:
-					<br>
-					${usuario.edicion_u == null ? " No actualizado" : usuario.edicion_u}
-					<br>
-					Registro:
-					<br>
-					${usuario.registro_u}
-					`,
-					// Agregar aquí las disciplinas del usuario
-					`
-                    <button class="btn btn-primary btn-editar-usuario" data-id="${usuario.id_usuario}" onclick="editarUsuario(${usuario.id_usuario})">Editar</button>
-                    `,
+					`Edición:<br>${
+						usuario.edicion_u == null ? "No actualizado" : usuario.edicion_u
+					}<br>Registro:<br>${usuario.registro_u}`,
+					`<button class="btn btn-primary btn-editar-usuario" data-id="${usuario.id_usuario}" onclick="editarUsuario(${usuario.id_usuario})">Editar</button>`,
 				])
 				.draw();
 		});
@@ -113,8 +125,6 @@ const showFichas = async () => {
 	}
 };
 
-// Función para editar un usuario
 function editarUsuario(id) {
-	// Aquí puedes implementar la lógica para editar el usuario con el ID proporcionado
 	console.log("Editar usuario con ID:", id);
 }
--- app/public/views/users/show-users.php
+++ app/public/views/users/show-users.php
@@ -10,33 +10,34 @@ include_once "app/view/templates/components/menu/menu.comp.php"; ?>
 <link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
 <link href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.bootstrap5.min.css" rel="stylesheet" />
 <link href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.bootstrap5.min.css" rel="stylesheet" />
-<div class="container mt-5">
-    <div class="row p-2">
+
+<div class="container mt-5 mb-5">
+    <div class="row">
         <div class="col-lg-12">
-            <div class="card mt-5 p-4">
+            <div class="card mb-2">
                 <div class="card-header text-center">
-                    <h5 class="card-title">USUARIOS</h5>
+                    <h5 class="card-title">USUARIOS | FEDEAV</h5>
                 </div>
                 <div class="card-body">
-                    <div class="table-responsive" style="max-width: 100%;">
+                    <div style="max-width: 80%;">
                         <!-- Agrega el estilo max-width -->
-                        <table id="tabla-usuarios" class="table table-striped" style="width:90%">
+                        <table id="tabla-usuarios" class="table table-striped table-responsive" style="max-width: 80%">
                             <thead>
                                 <tr>
                                     <th>Imágen</th>
-                                    <th>Nombres</th>
-                                    <th>Apellidos</th>
-                                    <th>Fecha de nacimiento</th>
-                                    <th>Sexo</th>
-                                    <th>Cédula</th>
-                                    <th>FEDEAV</th>
-                                    <th>INPRE</th>
-                                    <th>Contacto</th>
-                                    <th>Rol</th>
-                                    <th>Delegación</th>
-                                    <th>Disciplinas</th>
-                                    <th>Registro</th>
-                                    <th>Acciones</th>
+                                    <th>Nombres:</th>
+                                    <th>Apellidos:</th>
+                                    <th>Fecha de nacimiento:</th>
+                                    <th>Sexo:</th>
+                                    <th>Cédula:</th>
+                                    <th>FEDEAV:</th>
+                                    <th>INPRE:</th>
+                                    <th>Contacto:</th>
+                                    <th>Rol:</th>
+                                    <th>Delegación:</th>
+                                    <th>Disciplinas:</th>
+                                    <th>Registro:</th>
+                                    <th>Acciones:</th>
                                 </tr>
                             </thead>
                             <tbody>
