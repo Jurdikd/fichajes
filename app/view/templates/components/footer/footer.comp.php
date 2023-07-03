1d68e4e37e0e8aaf5e739dd817a75a269e5139db
Jurdikd <jesuscovo@gmail.com>
2023-07-03 12:44:58 -0400

    ajuste de texto

--- app/view/templates/components/footer/footer.comp.php
+++ app/view/templates/components/footer/footer.comp.php
@@ -16,19 +16,31 @@
             <div class="col-md-6">
                 <ul class="bottom_ul footer-ul bg-dark" dir="ltr">
                     <li><a href="<?php echo RUTA_GENERAL; ?>">Fichajes FEDEAV</a></li>
-                    <li><a href="mailto:contact@fichajefedeav.org?Subject=<?php echo RUTA_GENERAL; ?>%20Interesado%20en%20el%20servicio."><i class="fa fa-envelope"></i>contact@fichajefedeav.org</a></li>
-                    <li><a href="https://www.instagram.com/fedeavoficial" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i>@fedeavoficial</a></a></li>
+                    <li><a
+                            href="mailto:contact@fichajefedeav.org?Subject=<?php echo RUTA_GENERAL; ?>%20Interesado%20en%20el%20servicio."><i
+                                class="fa fa-envelope"></i>contact@fichajefedeav.org</a></li>
+                    <li><a href="https://www.instagram.com/fedeavoficial" target="_blank" rel="noopener noreferrer"><i
+                                class="fab fa-instagram"></i>@fedeavoficial</a></a></li>
                 </ul>
 
             </div>
             <div class="col-md-6 mx-auto">
                 <h4 class="text-center mt-2 text-white">POWERED BY:</h4>
                 <ul class="bottom_ul footer-ul bg-dark" dir="ltr">
-                    <li>Programador: <a href="https://api.whatsapp.com/send?phone=584245649007&text=Hola,%20¿Qué%20tal%20%estás?%20%quisiera%20%contratar%20%los%20%servicios%20%de%20%programación" target="_blank" rel="noopener noreferrer">instarapids.com</a>
+                    <li>
+                        <p class="text-white">Programador: <a
+                                href="https://api.whatsapp.com/send?phone=584245649007&text=Hola,%20¿Qué%20tal%20%estás?%20%quisiera%20%contratar%20%los%20%servicios%20%de%20%programación"
+                                target="_blank" rel="noopener noreferrer">instarapids.com</a></p>
                     </li>
-                    <li>Alojamiento: <a href="https://api.whatsapp.com/send?phone=584145681386&text=Hola,%20¿Qué%20tal%20%estás?%20%quisiera%20%contratar%20%los%20%servicios%20%de%20%alojamiento%20%web" target="_blank" rel="noopener noreferrer">guslaya.com</a>
+                    <li>
+                        <p class="text-white">Alojamiento: <a
+                                href="https://api.whatsapp.com/send?phone=584145681386&text=Hola,%20¿Qué%20tal%20%estás?%20%quisiera%20%contratar%20%los%20%servicios%20%de%20%alojamiento%20%web"
+                                target="_blank" rel="noopener noreferrer">guslaya.com</a></p>
                     </li>
-                    <li>Diseños: <a href="https://api.whatsapp.com/send?phone=584249679522&text=Hola,%20¿Qué%20tal%20%estás?%20%quisiera%20%contratar%20%los%20%servicios%20%de%20%diseño" target="_blank" rel="noopener noreferrer">luyadesing</a>
+                    <li>
+                        <p class="text-white">Diseños: <a
+                                href="https://api.whatsapp.com/send?phone=584249679522&text=Hola,%20¿Qué%20tal%20%estás?%20%quisiera%20%contratar%20%los%20%servicios%20%de%20%diseño"
+                                target="_blank" rel="noopener noreferrer">luyadesing</a></p>
                     </li>
                 </ul>
             </div>
