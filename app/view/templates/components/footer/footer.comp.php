e254a7d5585e73b0484fe1501d4e99ff031bed9d
Jesus Covo <jesuscovo@gmail.com>
2024-05-06 10:22:35 -0400

    Correción de footer.comp.php
    Ajuste para instagram

--- app/view/templates/components/footer/footer.comp.php
+++ app/view/templates/components/footer/footer.comp.php
@@ -64,9 +64,9 @@
                         <span class="text-white">Programador: Copyright © 2023 / 2024</span> <a
                             href="https://api.whatsapp.com/send?phone=584245649007&text=Hola,%20%C2%BFQu%C3%A9%20tal%20est%C3%A1s?%20Quisiera%20contratar%20los%20servicios%20de%20programaci%C3%B3n"
                             target="_blank" rel="noopener noreferrer">JESUS A. COVO GARRIDO</a>
-                            <span class="text-white"> @jesuscovo777</span>
+                            
                             <a href="https://www.instagram.com/jesuscovo777/" target="_blank">
-          <i class="bi bi-instagram"></i> Seguir en Instagram
+          <i class="fa fas-instagram"></i> @jesuscovo777
         </a>
                     </li>
                     <li>
