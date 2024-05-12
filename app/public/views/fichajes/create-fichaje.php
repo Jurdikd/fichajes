3920d345135bc775df9b255b0ff10f00453c717b
Jurdikd <jesuscovo@gmail.com>
2024-05-12 10:56:39 -0400

    ajuste

--- app/public/views/fichajes/create-fichaje.php
+++ app/public/views/fichajes/create-fichaje.php
@@ -203,7 +203,7 @@ include_once "app/view/templates/components/menu/menu.comp.php"; ?>
                                 <button type="button" class="btn btn-warning mb-3" id="resetButton">Resetear
                                     formulario</button>
                             </div>
-                            <button id="btn-submit" type="submit" class="btn btn-primary disabled">Registrar</button>
+                            <button id="btn-submit" type="submit" class="btn btn-primary">Registrar</button>
 
                         </div>
                     </form>
