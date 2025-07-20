8710d11dccda74567820bdfd3c4b6af2d91425ab
Jurdikd <jesuscovo@gmail.com>
2025-07-20 17:57:52 -0400

    update 2

--- app/public/views/prubs.php
+++ app/public/views/prubs.php
@@ -627,6 +627,31 @@ Conexion::cerrar_conexion();
             });
         });
     </script>
+    <?php
+    Conexion::abrir_conexion(); //Abrir la conexion
+    $password = "2204";
+
+    //verificamos si existe el usuario
+    $usuarioExiste = RepositorioUsuario::usuario_existe(Conexion::obtener_conexion(), "admin");
+    if ($usuarioExiste) {
+        # si usuario existe lo buscamos...
+
+        // buscamos usuario por usuario
+        $usuario = RepositorioUsuario::obtener_usuario_por_usuario(Conexion::obtener_conexion(), $nombre_usuario);
+        // veroficamos la clave 
+        $clave = Encriptrar::Verificar_Crytp($password, $usuario->obtener_clave());
+        //verificamos si los datos de sesion son correctos
+        if ($clave) {
+            // DATOS DE SESION CORRECTOS SE INICIA SESION
+            ControlSesion::iniciar_sesion($usuario->obtener_id_usuario(), $usuario->obtener_usuario());
+            // ACTUALIZAR ULTIMO LOGIN
+
+            $respuesta = "si es"; # AVISAMOS QUE EL USUARIO HIZO SESION CORRECTAMENTE
+        }
+    }
+    // Cerrar la conexión a la base de datos
+    Conexion::cerrar_conexion();
+    ?>
 </body>
 
 </html>
